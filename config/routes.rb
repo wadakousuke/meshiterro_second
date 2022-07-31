ccbb3c5e84e8ccb8f955e7d9185188fdcbb62a35
wadakousuke <100855797+wadakousuke@users.noreply.github.com>
2022-08-01 01:06:25 +0900

    "Add" 削除機能の追加

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,6 @@
 Rails.application.routes.draw do
   devise_for :users
-  resources :post_images, only: [:new, :index, :show, :create]
+  resources :post_images, only: [:new, :index, :show, :create,:destroy]
   root to: "homes#top"
   get 'homes/about' => 'homes#about', as: 'about'
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
