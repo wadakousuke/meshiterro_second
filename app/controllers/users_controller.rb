872778e7bb2f28a6a5cd0dd39061b530e3c8c233
wadakousuke <100855797+wadakousuke@users.noreply.github.com>
2022-08-03 23:55:40 +0900

    "Add" ユーザー情報編集機能の追加

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -7,4 +7,15 @@ class UsersController < ApplicationController
     @user = User.find(params[:id])
     @post_images = @user.post_images
   end
+
+  def update
+    @user = User.find(params[:id])
+    @user.update(user_params)
+    redirect_to user_path(@user.id)
+  end
+
+  private
+   def user_params
+    params.require(:user).permit(:name)
+   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,7 @@ Rails.application.routes.draw do
  
   devise_for :users
   resources :post_images, only: [:new, :index, :show, :create,:destroy]
-  resources :users, only: [:edit,:show]
+  resources :users, only: [:edit,:show, :update]
   root to: "homes#top"
   get 'homes/about' => 'homes#about', as: 'about'
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
