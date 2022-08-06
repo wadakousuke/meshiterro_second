f00365bdc61eaedbb9e4262c223797872c83deff
wadakousuke <100855797+wadakousuke@users.noreply.github.com>
2022-08-06 22:39:51 +0900

    "Add" 部分テンプレートの追加

--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,7 @@ Rails.application.routes.draw do
  
   devise_for :users
   resources :post_images, only: [:new, :index, :show, :create,:destroy] do
+    resources :favorites, only:[:create, :destroy]
     resources :post_comments, only:[:create, :destroy]
   end
   resources :users, only: [:edit,:show, :update]
